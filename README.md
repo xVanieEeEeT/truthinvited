6d3909d15b730bf0374301992539c7b79aa7e17c
xVanieEeEeT <42171095+xVanieEeEeT@users.noreply.github.com>
2018-10-24 22:37:06 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# truthinvited
+# starsinvited
---
+++ bot.js
@@ -0,0 +1,163 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = 'lU';
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setGame("United. || Invite","https://twitch.tv//9ivv")
+    console.log('')
+    console.log('')
+    console.log('╔[════════════════════════════════════════════════════════════════]╗')
+    console.log(`[Start] ${new Date()}`);
+    console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+    console.log('')
+    console.log('╔[════════════════════════════════════]╗');
+    console.log(`Logged i as * [ " ${client.user.username} " ]`);
+    console.log('')
+    console.log('Informatins :')
+    console.log('')
+    console.log(`servers! [ " ${client.guilds.size} " ]`);
+    console.log(`Users! [ " ${client.users.size} " ]`);
+    console.log(`channels! [ " ${client.channels.size} " ]`);
+    console.log('╚[════════════════════════════════════]╝')
+    console.log('')
+    console.log('╔[════════════]╗')
+    console.log(' Bot Is Online')
+    console.log('╚[════════════]╝')
+    console.log('')
+    console.log('')
+  });
+
+  client.on('message', message => {
+
+	if(message.author.id === "474175378118803466" || message.author.id === "410778583682777098") {
+    if (!message.content.startsWith(prefix)) return;
+    var args = message.content.split(' ').slice(1);
+    var argresult = args.join(' ');
+
+
+    if (message.content.startsWith(prefix + 'setwatch')) {
+    client.user.setActivity(argresult, {type: 'WATCHING'})
+       console.log('test' + argresult);
+      message.channel.sendMessage(`Watching Now: **${argresult}**`)
+  }
+
+
+    if (message.content.startsWith(prefix + 'setlis')) {
+    client.user.setActivity(argresult, {type: 'LISTENING'})
+       console.log('test' + argresult);
+      message.channel.sendMessage(`LISTENING Now: **${argresult}**`)
+  }
+
+
+  if (message.content.startsWith(prefix + 'setname')) {
+    client.user.setUsername(argresult).then
+        message.channel.sendMessage(`Username Changed To **${argresult}**`)
+    return message.reply(".");
+  }
+
+  if (message.content.startsWith(prefix + 'setavatar')) {
+    client.user.setAvatar(argresult);
+     message.channel.sendMessage(`Avatar Changed Successfully To **${argresult}**`);
+  }
+
+  if (message.content.startsWith(prefix + 'setstream')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/9ivv");
+       console.log('test' + argresult);
+      message.channel.sendMessage(`Streaming: **${argresult}**`)
+  }
+  if (message.content.startsWith(prefix + 'setplay')) {
+    client.user.setGame(argresult);
+       console.log('test' + argresult);
+      message.channel.sendMessage(`Playing: **${argresult}**`)
+  }
+	}
+
+ });
+ 
+ client.on('guildMemberAdd', member => {
+  member.guild.fetchInvites().then(guildInvites => {
+    const ei = invites[member.guild.id];
+    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
+    const inviter = client.users.get(invite.inviter.id);
+    const vanieeet = member.guild.channels.find("name", "ask");
+     vanieeet.send(`**. <@${member.user.id}> , تمت دعـوته بواسطهه <@${inviter.id}> 🥂 .**`);
+  }); 
+});
+
+
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) });
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+    var guild = client.guilds.get("504639640796397571");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        });
+    });
+});
+ 
+ client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find(c => c.name === 'truth');
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+     var guild = client.guilds.get("504639640796397571");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    setTimeout(function() {
+                        channel.send(`**${member},\n تمت دعـوته بواسطهه ${Invite.inviter} 🥂 .**`) ;
+                    },1500);
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
+
+
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find(c => c.name === 'truth');
+    if (!channel) {
+        console.log("!the channel id it's not correct");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('-');
+    var guild;
+    while (!guild)
+        var guild = client.guilds.get("504639640796397571");
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    setTimeout(function() {
+ channel.send(`**${member},\n تمت دعـوته بواسطهه ${Invite.inviter} 🥂 .**`) ;
+                    },1500);
+ }
+            dat[Inv] = Invite.uses;
+       
+       });
+    });
+});
+
+
+client.login(process.env.BOT_TOKEN);
